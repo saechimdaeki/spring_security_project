71659c70996eb21bee8a82a583e63a84ae8dde1e
Junseong kim <anima94@kakao.com>
2022-05-04 17:16:30 +0900

    customLoginPage with SpringSecurity

--- src/main/java/me/saechimdaeki/security/security/config/SecurityConfig.java
+++ src/main/java/me/saechimdaeki/security/security/config/SecurityConfig.java
@@ -59,6 +59,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated()
 
             .and()
-            .formLogin();
+            .formLogin()
+            .loginPage("/login")
+            .loginProcessingUrl("/login_proc")
+            .defaultSuccessUrl("/",true)
+            .permitAll()
+        ;
     }
 }
