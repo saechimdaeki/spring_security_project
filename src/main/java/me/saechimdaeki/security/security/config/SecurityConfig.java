0bb0c9f9c4eb487be5f1bd7645f7c7c026d99ab6
Junseong kim <anima94@kakao.com>
2022-05-08 12:57:19 +0900

    AjaxAuthenticationProvider

--- src/main/java/me/saechimdaeki/security/security/config/SecurityConfig.java
+++ src/main/java/me/saechimdaeki/security/security/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import me.saechimdaeki.security.security.service.CustomUserDetails;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -25,6 +26,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@Order(1)
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -81,10 +83,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .permitAll()
         .and().exceptionHandling()
             .accessDeniedHandler(accessDeniedHandler())
-        .and()
-            .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
         ;
-        http.csrf().disable();
     }
 
     @Bean
@@ -95,10 +94,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Bean
-    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
-        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
-        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
-        return ajaxLoginProcessingFilter;
-    }
+
 }
---
+++ src/main/java/me/saechimdaeki/security/security/provider/AjaxAuthenticationProvider.java
@@ -0,0 +1,51 @@
+package me.saechimdaeki.security.security.provider;
+
+import lombok.RequiredArgsConstructor;
+import me.saechimdaeki.security.security.common.FormWebAuthenticationDetails;
+import me.saechimdaeki.security.security.service.AccountContext;
+import me.saechimdaeki.security.security.service.CustomUserDetails;
+import me.saechimdaeki.security.security.token.AjaxAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AjaxAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomUserDetails userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String password = (String)authentication.getCredentials();
+
+        AccountContext accountContext = (AccountContext) userDetailsService.loadUserByUsername(username);
+
+        if(!passwordEncoder.matches(password, accountContext.getAccount().getPassword())){
+            throw new BadCredentialsException("BadCredentials Exception!!!");
+        }
+
+        FormWebAuthenticationDetails formWebAuthenticationDetails = (FormWebAuthenticationDetails)authentication.getDetails();
+        String secretKey = formWebAuthenticationDetails.getSecretKey();
+        if(!"secret".equals(secretKey)){
+            throw new IllegalArgumentException("Invalid Secret");
+        }
+
+        return new AjaxAuthenticationToken(accountContext.getAccount(), null , accountContext.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(AjaxAuthenticationToken.class);
+    }
+}
