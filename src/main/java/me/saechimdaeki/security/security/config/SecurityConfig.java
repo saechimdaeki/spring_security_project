bacf04876652da678230701a0bd446af2eff5b33
Junseong kim <anima94@kakao.com>
2022-05-14 13:24:49 +0900

    EnableGlobalMethodSecurity

--- src/main/java/me/saechimdaeki/security/security/config/SecurityConfig.java
+++ src/main/java/me/saechimdaeki/security/security/config/SecurityConfig.java
@@ -26,6 +26,7 @@ import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -48,6 +49,7 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @Slf4j
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
